9848fda861d7b76bc732a59d6337577fce5686eb
Terry Bates <terryjbates@gmail.com>
2015-07-09 23:53:18 -0700

    Added personal solution and recommened Udacity solution as comments.

--- lesson_2/authors.py
+++ lesson_2/authors.py
@@ -23,7 +23,10 @@ def get_authors(root):
         data['fnm'] = author.find('fnm').text
         data['snm'] = author.find('snm').text
         data['email'] = author.find('email').text
-
+        # Find insr tag
+        for insr_item in author.findall('insr'):
+            insr = insr_item.get('iid')
+            data['insr'].append(insr)
         authors.append(data)
 
     return authors
@@ -40,3 +43,24 @@ def test():
 
 
 test()
+
+
+# Proposed Solution
+# def get_authors(root):
+#     authors = []
+#     for author in root.findall('./fm/bibl/aug/au'):
+#         data = {
+#                 "fnm": None,
+#                 "snm": None,
+#                 "email": None,
+#                 "insr": []
+#         }
+#         data["fnm"] = author.find('./fnm').text
+#         data["snm"] = author.find('./snm').text
+#         data["email"] = author.find('./email').text
+#         insr = author.findall('./insr')
+#         for i in insr:
+#             data["insr"].append(i.attrib["iid"])
+#         authors.append(data)
+#
+#     return authors
